0ac245af253b7bb201af279d9e1d95d41bf45709
Maximilian Bonengl <mbonengl@c3r3p6.42vienna.com>
2024-11-05 16:40:12 +0100

    some fixes

--- src/execution/wrappers_open.c
+++ src/execution/wrappers_open.c
@@ -6,7 +6,7 @@
 /*   By: mbonengl <mbonengl@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 14:48:34 by mbonengl          #+#    #+#             */
-/*   Updated: 2024/11/05 14:22:26 by mbonengl         ###   ########.fr       */
+/*   Updated: 2024/11/05 16:35:53 by mbonengl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,27 @@ static void	open_error(t_msh *msh, int errorcode, char *path)
 	if (errorcode == EACCES)
 		error_complex(msh, O_PER_ERR, path, 1);
 	else if (errorcode == EEXIST)
-		error_complex(msh, ": file exists", path, 1);
+		error_complex(msh, ": File exists ", path, 1);
 	else if (errorcode == EISDIR)
-		error_complex(msh, ": is a directory", path, 1);
+		error_complex(msh, ": Is a directory ", path, 1);
 	else if (errorcode == ENOMEM)
 		error_simple(msh, M_ERR, 1);
 	else if (errorcode == EROFS)
-		error_complex(msh, ": read-only file system", path, 1);
+		error_complex(msh, ": Read-only file system ", path, 1);
 	else if (errorcode == ELOOP)
-		error_complex(msh, ": too many levels of symbolic links", path, 1);
+		error_complex(msh, ": Too many levels of symbolic links ", path, 1);
 	else if (errorcode == ENAMETOOLONG)
-		error_complex(msh, ": file name too long", path, 1);
+		error_complex(msh, ": File name too long ", path, 1);
 	else if (errorcode == EMFILE)
-		error_complex(msh, ": too many open files", path, 1);
+		error_complex(msh, ": Too many open files ", path, 1);
 	else if (errorcode == EFAULT)
-		error_complex(msh, ": bad address", path, 1);
+		error_complex(msh, ": Bad address", path, 1);
 	else if (errorcode == EINTR)
-		error_complex(msh, ": interrupted function call", path, 1);
+		error_complex(msh, ": Interrupted function call ", path, 1);
 	else if (errorcode == ENOSPC)
-		error_complex(msh, ": no space left on device", path, 1);
+		error_complex(msh, ": No space left on device ", path, 1);
 	else
-		error_complex(msh, ": no such file or directory", path, 1);
+		error_complex(msh, ": No such file or directory ", path, 1);
 }
 
 static void	open_error_builtin(t_msh *msh, int errorcode, char *path)
@@ -45,27 +45,27 @@ static void	open_error_builtin(t_msh *msh, int errorcode, char *path)
 	if (errorcode == EACCES)
 		dis_func_err("", path, O_PER_ERR);
 	else if (errorcode == EEXIST)
-		dis_func_err("", path, ": file exists");
+		dis_func_err("", path, ": File exists");
 	else if (errorcode == EISDIR)
-		dis_func_err("", path, ": is a directory");
+		dis_func_err("", path, ": Is a directory");
 	else if (errorcode == ENOMEM)
 		error_simple(msh, M_ERR, 1);
 	else if (errorcode == EROFS)
-		dis_func_err("", path, ": read-only file system");
+		dis_func_err("", path, ": Read-only file system");
 	else if (errorcode == ELOOP)
-		dis_func_err("", path, ": too many levels of symbolic links");
+		dis_func_err("", path, ": Too many levels of symbolic links");
 	else if (errorcode == ENAMETOOLONG)
-		dis_func_err("", path, ": file name too long");
+		dis_func_err("", path, ": Gile name too long");
 	else if (errorcode == EMFILE)
-		dis_func_err("", path, ": too many open files");
+		dis_func_err("", path, ": Too many open files");
 	else if (errorcode == EFAULT)
 		dis_func_err("", path, ": bad address");
 	else if (errorcode == EINTR)
-		dis_func_err("", path, ": interrupted function call");
+		dis_func_err("", path, ": Interrupted function call");
 	else if (errorcode == ENOSPC)
-		dis_func_err("", path, ": no space left on device");
+		dis_func_err("", path, ": No space left on device");
 	else
-		dis_func_err("", path, ": no such file or directory");
+		dis_func_err("", path, ": No such file or directory");
 }
 
 int	wrppd_open(t_msh *msh, char *path, int flags)
